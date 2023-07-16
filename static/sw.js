be493a25688d20a2fdebc6f29aef67b3242f1a74
nailuoGG <nailuogg@gmail.com>
2023-07-16 12:47:11 +0800

    feat: 增加service worker离线缓存的支持

---
+++ static/sw.js
@@ -0,0 +1,65 @@
+/**
+ * service worker 安装激活
+ */
+
+let dataCacheName = 'new-data-v1'
+let cacheName = 'first-pwa-app-1'
+let filesToCache = [
+  '/',
+  '/posts/',
+  '/categories/',
+  '/resume/',
+  '/js/jquery-3.5.1.min.js',
+  '/js/fancybox.min.js',
+  '/images/favicon.ico'
+]
+
+self.addEventListener('install', function (e) {
+  console.log('SW Install')
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log('SW precaching')
+      return cache.addAll(filesToCache)
+    })
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', function (e) {
+  console.log('SW Activate')
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('SW Removing old cache', key)
+          return caches.delete(key)
+        }
+      }))
+    })
+  )
+  return self.clients.claim()
+})
+
+self.addEventListener('fetch', function (e) {
+  console.log('SW Fetch', e.request.url)
+  // 如果数据相关的请求，需要请求更新缓存
+  let dataUrl = '/mockData/'
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    e.respondWith(
+      caches.open(dataCacheName).then(function (cache) {
+        return fetch(e.request).then(function (response) {
+          cache.put(e.request.url, response.clone())
+          return response
+        }).catch(function () {
+          return caches.match(e.request)
+        })
+      })
+    )
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function (response) {
+        return response || fetch(e.request)
+      })
+    )
+  }
+})
