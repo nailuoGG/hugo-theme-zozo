6a415cc6babef8f69ce4fd65debcd65e34c7053f
nailuoGG <nailuogg@gmail.com>
2023-07-16 13:30:40 +0800

    chore: 增加 pwa 桌面图标

--- static/sw.js
+++ static/sw.js
@@ -2,12 +2,23 @@
  * service worker 安装激活
  */
 
+let CACHE_VERSION = 1;
+
+const CACHE_VERSIONS = {
+  assets: 'assets-v' + CACHE_VERSION,
+  content: 'content-v' + CACHE_VERSION,
+  offline: 'offline-v' + CACHE_VERSION,
+  notFound: '404-v' + CACHE_VERSION,
+};
+
 let dataCacheName = 'new-data-v1'
 let cacheName = 'first-pwa-app-1'
 let filesToCache = [
   '/',
   '/posts/',
   '/categories/',
+  '/tags/',
+  '/about/',
   '/resume/',
   '/js/jquery-3.5.1.min.js',
   '/js/fancybox.min.js',
@@ -17,7 +28,7 @@ let filesToCache = [
 self.addEventListener('install', function (e) {
   console.log('SW Install')
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(CACHE_VERSIONS.assets).then(function (cache) {
       console.log('SW precaching')
       return cache.addAll(filesToCache)
     })
@@ -30,7 +41,7 @@ self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       return Promise.all(keyList.map(function (key) {
-        if (key !== cacheName && key !== dataCacheName) {
+        if (key !== CACHE_VERSIONS.assets && key !== CACHE_VERSION.content) {
           console.log('SW Removing old cache', key)
           return caches.delete(key)
         }
@@ -43,10 +54,11 @@ self.addEventListener('activate', function (e) {
 self.addEventListener('fetch', function (e) {
   console.log('SW Fetch', e.request.url)
   // 如果数据相关的请求，需要请求更新缓存
-  let dataUrl = '/mockData/'
+  let dataUrl = '/see/'
   if (e.request.url.indexOf(dataUrl) > -1) {
+    console.log('see不缓存');
     e.respondWith(
-      caches.open(dataCacheName).then(function (cache) {
+      caches.open(CACHE_VERSIONS.content).then(function (cache) {
         return fetch(e.request).then(function (response) {
           cache.put(e.request.url, response.clone())
           return response
