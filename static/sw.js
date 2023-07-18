9827834ee0551d9c93723bf44979ae0572b9165c
nailuoGG <nailuogg@gmail.com>
2023-07-18 13:35:55 +0800

    chore: 优化离线缓存逻辑

--- static/sw.js
+++ static/sw.js
@@ -4,74 +4,104 @@
 
 let CACHE_VERSION = "1.0.1";
 
-const CACHE_VERSIONS = {
-  assets: 'assets-v' + CACHE_VERSION,
-  content: 'content-v' + CACHE_VERSION,
-  offline: 'offline-v' + CACHE_VERSION,
-  notFound: '404-v' + CACHE_VERSION,
-};
+
+const getCache = function() {
+    const CACHE_VERSIONS = {
+        assets: 'assets-v' + CACHE_VERSION,
+        content: 'content-v' + CACHE_VERSION,
+        offline: 'offline-v' + CACHE_VERSION,
+        notFound: '404-v' + CACHE_VERSION,
+    };
+    return CACHE_VERSIONS
+}
 
 let dataCacheName = 'new-data-v1'
 let cacheName = 'first-pwa-app-1'
 let filesToCache = [
-  '/',
-  '/posts/',
-  '/categories/',
-  '/tags/',
-  '/about/',
-  '/resume/',
-  '/js/jquery-3.5.1.min.js',
-  '/js/fancybox.min.js',
-  '/images/favicon.ico'
+    '/',
+    '/posts/',
+    '/categories/',
+    '/tags/',
+    '/about/',
+    '/resume/',
+    '/js/jquery-3.5.1.min.js',
+    '/js/fancybox.min.js',
+    '/images/favicon.ico'
 ]
 
-self.addEventListener('install', function (e) {
-  console.log('SW Install')
-  e.waitUntil(
-    caches.open(CACHE_VERSIONS.assets).then(function (cache) {
-      console.log('SW precaching')
-      return cache.addAll(filesToCache)
-    })
-  )
-  self.skipWaiting()
+self.addEventListener('install', function(e) {
+    console.log('SW Install')
+    // e.waitUntil(
+    //   caches.open(CACHE_VERSIONS.assets).then(function (cache) {
+    //     console.log('SW precaching')
+    //     return cache.addAll(filesToCache)
+    //   })
+    // )
+    self.skipWaiting()
 })
 
-self.addEventListener('activate', function (e) {
-  console.log('SW Activate')
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
-        if (key !== CACHE_VERSIONS.assets && key !== CACHE_VERSION.content) {
-          console.log('SW Removing old cache', key)
-          return caches.delete(key)
-        }
-      }))
-    })
-  )
-  return self.clients.claim()
+self.addEventListener('activate', function(e) {
+    console.log('SW Activate')
+    // e.waitUntil(
+    //     caches.keys().then(function(keyList) {
+    //         return Promise.all(keyList.map(function(key) {
+    //             const CACHE_VERSIONS = getCache();
+    //             if (key !== CACHE_VERSIONS.assets && key !== CACHE_VERSION.content) {
+    //                 console.log('SW Removing old cache', key)
+    //                 return caches.delete(key)
+    //             }
+    //         }))
+    //     })
+    // )
+    return self.clients.claim()
 })
 
-self.addEventListener('fetch', function (e) {
-  console.log('SW Fetch', e.request.url)
-  // 如果数据相关的请求，需要请求更新缓存
-  let dataUrl = '/see/'
-  if (e.request.url.indexOf(dataUrl) > -1) {
-    console.log('see不缓存');
-    e.respondWith(
-      caches.open(CACHE_VERSIONS.content).then(function (cache) {
-        return fetch(e.request).then(function (response) {
-          cache.put(e.request.url, response.clone())
-          return response
-        }).catch(function () {
-          return caches.match(e.request)
+self.addEventListener('fetch', function(e) {
+    console.log('SW Fetch', e.request.url)
+    // 如果数据相关的请求，需要请求更新缓存
+    let dataUrl = '/see/'
+    if (e.request.url.indexOf(dataUrl) > -1) {
+        console.log('see不缓存');
+        const CACHE_VERSIONS = getCache();
+        e.respondWith(
+            caches.open(CACHE_VERSIONS.content).then(function(cache) {
+                return fetch(e.request).then(function(response) {
+                    cache.put(e.request.url, response.clone())
+                    return response
+                }).catch(function() {
+                    return caches.match(e.request)
+                })
+            })
+        )
+    } else {
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request)
+            })
+        )
+    }
+})
+
+self.addEventListener('message', function(e) {
+    console.log('--------message from page', e)
+    var urls = e.data.urls;
+    var gitHash = e.data.gitHash;
+    CACHE_VERSION = gitHash;
+    if (e.data.action === 'cache') {
+        const CACHE_VERSIONS = getCache();
+        caches.open(CACHE_VERSIONS.assets).then(function(cache) {
+            console.log('SW precaching urls')
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                const CACHE_VERSIONS = getCache();
+                if (key !== CACHE_VERSIONS.assets && key !== CACHE_VERSION.content) {
+                    console.log('SW Removing old cache', key)
+                    return caches.delete(key)
+                }
+            }))
         })
-      })
-    )
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(function (response) {
-        return response || fetch(e.request)
-      })
-    )
-  }
+        cache.addAll(urls);
+        return self.clients.claim();
+        })
+    }
 })
