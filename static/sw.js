4325ac15b3a7382c402b94f009fa445f9eabf487
nailuoGG <nailuogg@gmail.com>
2023-07-20 13:49:41 +0800

    chore: 优化图片区域的光标属性

--- static/sw.js
+++ static/sw.js
@@ -5,12 +5,12 @@
 let CACHE_VERSION = "1.0.0";
 
 
-  var CACHE_VERSIONS = {
-        assets: 'assets-v' + CACHE_VERSION,
-        content: 'content-v' + CACHE_VERSION,
-        offline: 'offline-v' + CACHE_VERSION,
-        notFound: '404-v' + CACHE_VERSION,
-    };
+var CACHE_VERSIONS = {
+    assets: 'assets-v' + CACHE_VERSION,
+    content: 'content-v' + CACHE_VERSION,
+    offline: 'offline-v' + CACHE_VERSION,
+    notFound: '404-v' + CACHE_VERSION,
+};
 
 let dataCacheName = 'new-data-v1'
 let cacheName = 'first-pwa-app-1'
@@ -26,7 +26,7 @@ let filesToCache = [
     '/images/favicon.ico'
 ]
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     console.log('SW Install')
     // e.waitUntil(
     //   caches.open(CACHE_VERSIONS.assets).then(function (cache) {
@@ -37,11 +37,11 @@ self.addEventListener('install', function(e) {
     self.skipWaiting()
 })
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
     console.log('SW Activate')
     e.waitUntil(
-        caches.keys().then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
                 if (key !== CACHE_VERSIONS.assets && key !== CACHE_VERSION.content) {
                     console.log('SW Removing old cache', key)
                     return caches.delete(key)
@@ -52,37 +52,37 @@ self.addEventListener('activate', function(e) {
     return self.clients.claim()
 })
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
     console.log('SW Fetch', e.request.url)
     // 如果数据相关的请求，需要请求更新缓存
     let dataUrl = '/see/'
-    if (e.request.url.indexOf(dataUrl) > -1) {
+    if (e.request.url.indexOf(dataUrl) > -1 || e.request.url.indexOf('/livereload/') > -1) {
         console.log('see不缓存');
 
         e.respondWith(
-            caches.open(CACHE_VERSIONS.content).then(function(cache) {
-                return fetch(e.request).then(function(response) {
+            caches.open(CACHE_VERSIONS.content).then(function (cache) {
+                return fetch(e.request).then(function (response) {
                     cache.put(e.request.url, response.clone())
                     return response
-                }).catch(function() {
+                }).catch(function () {
                     return caches.match(e.request)
                 })
             })
         )
     } else {
         e.respondWith(
-            caches.match(e.request).then(function(response) {
+            caches.match(e.request).then(function (response) {
                 return response || fetch(e.request)
             })
         )
     }
 })
 
-self.addEventListener('message', function(e) {
+self.addEventListener('message', function (e) {
     var urls = e.data.urls;
     if (e.data.action === 'cache') {
-        caches.open(CACHE_VERSIONS.assets).then(function(cache) {
-        cache.addAll(urls);
+        caches.open(CACHE_VERSIONS.assets).then(function (cache) {
+            cache.addAll(urls);
         })
     }
 })
